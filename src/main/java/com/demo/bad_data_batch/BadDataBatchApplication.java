e6fe53ae1001861ab87ae5d34507e9b6b9644247
Ted Erickson <ted.erickson@comcast.net>
2024-10-10 12:28:03 -0600

    initial commit

---
+++ src/main/java/com/demo/bad_data_batch/BadDataBatchApplication.java
@@ -0,0 +1,13 @@
+package com.demo.bad_data_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadDataBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadDataBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/bad_data_batch/BadDataBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.bad_data_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadDataBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
