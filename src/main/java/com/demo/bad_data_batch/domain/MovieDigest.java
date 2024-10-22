617bd30319f44b48e170e3da9802f450edbb766b
Ted Erickson <ted.erickson@comcast.net>
2024-10-22 10:20:47 -0600

    Simplify MovieDigest

--- src/main/java/com/demo/bad_data_batch/domain/MovieDigest.java
+++ src/main/java/com/demo/bad_data_batch/domain/MovieDigest.java
@@ -1,6 +1,3 @@
 package com.demo.bad_data_batch.domain;
 
-import lombok.Builder;
-
-@Builder
 public record MovieDigest(Long id, String title, Integer year) {}
--- src/main/java/com/demo/bad_data_batch/mapper/ModelMapper.java
+++ src/main/java/com/demo/bad_data_batch/mapper/ModelMapper.java
@@ -5,10 +5,6 @@ import com.demo.bad_data_batch.model.Movie;
 
 public class ModelMapper {
     public static MovieDigest toRest(final Movie movie) {
-        return MovieDigest.builder()
-                .id(movie.getId())
-                .title(movie.getTitle())
-                .year(movie.getYear())
-                .build();
+        return new MovieDigest(movie.getId(), movie.getTitle(), movie.getYear());
     }
 }
